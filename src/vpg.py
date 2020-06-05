4ce0c4e318db7bbef8fefb2ec43f0cfbc87dc082
kyo-kutsuzawa <k.kutsuzawa.430@gmail.com>
2020-06-05 10:33:03 +0900

    Refactor the vpg code

--- src/vpg.py
+++ src/vpg.py
@@ -11,79 +11,19 @@ from spinup import vpg_pytorch as vpg
 import spinup.algos.pytorch.vpg.core as core
 
 
-class VPGAgent:
-    def __init__(self, act_dim, obs_dim, n_units_pi, n_units_v):
-        self.pi = Pi(obs_dim, act_dim, n_units_pi)
-        self.v = V(obs_dim, 1, n_units_v)
-
-    def step(self, obs):
-        a = self.pi(obs)
-        v = self.v(obs)
-        logp_a = None
-
-        return a, v, logp_a
-
-    def act(self, obs):
-        a = self.pi(obs)
-        return a
-
-
-class Pi(nn.Module):
-    def __init__(self, in_size, out_size, n_units):
-        super(Pi, self).__init__()
-
-        self.l1 = nn.Linear(in_size, n_units)
-        self.l2 = nn.Linear(n_units, n_units)
-        self.l3 = nn.Linear(n_units, out_size)
-
-    def forward(self, obs, a=None):
-        h = F.sigmoid(self.l1(obs))
-        h = F.sigmoid(self.l2(h))
-        logp_a = F.relu(self.l3(h))
-
-        if a is not None:
-            return logp_a, self
-
-        return logp_a
-
-
-class V(nn.Module):
-    def __init__(self, in_size, out_size, n_units):
-        super(V, self).__init__()
-
-        self.l1 = nn.Linear(in_size, n_units)
-        self.l2 = nn.Linear(n_units, n_units)
-        self.l3 = nn.Linear(n_units, out_size)
-
-    def forward(self, x):
-        h = F.sigmoid(self.l1(x))
-        h = F.sigmoid(self.l2(h))
-        y = self.l3(h)
-        return y
-
-
-def make_actor_critic(action_space, obs_space, **kwargs):
-    act_dim = action_space.shape[-1]
-    obs_dim = obs_space.shape[-1]
-    
-    model = VPGAgent(act_dim, obs_dim, **kwargs)
-    return model
-
-
 def main():
     import argparse
 
     # Process commandline arguments
     parser = argparse.ArgumentParser(description="Example of Gym + PyTorch")
     parser.add_argument('--mode', choices=['train', 'test'], default='train', help='Whether training or test')
-    parser.add_argument('--n-episodes', type=int, default=100, help='Number of episodes')
-    parser.add_argument('--max-steps', type=int, default=500, help='Number of max. steps for an episode')
+    parser.add_argument('--n-episodes', type=int, default=1000, help='Number of episodes')
+    parser.add_argument('--max-steps', type=int, default=200, help='Number of max. steps for an episode')
     parser.add_argument('--render', action='store_true', help='If specified, render the environment')
     parser.add_argument('--out', type=str, default='results', help='Output directory')
     args = parser.parse_args()
 
     # Define settings
-    env_name = 'MountainCarContinuous-v0'
     env_name = 'Pendulum-v0'
 
     # Setup parameters
@@ -92,7 +32,6 @@ def main():
         'activation': nn.Tanh
     }
     vpg_params = {
-        #'env_fn': lambda : gym.make(env_name),
         'env_fn': lambda: wrappers.Monitor(gym.make(env_name), directory=args.out, force=True),
         'ac_kwargs': model_params,
         'seed': 0,
